defa634d4bea00449fee6f957f608bea4ba9cb53
yuleidimonteroobando <yuleidimontero28@gmail.com>
2025-08-15 09:31:54 +0200

    java

---
+++ prompt.js
@@ -0,0 +1,5 @@
+let palabra1 = prompt("digite palabra1 a conetar");
+let palabra2 = prompt("digite palabra2 a conetar");
+let conetar = palabra1 + palabra2;
+console.log(conetar)
+alert("esta es la palabra conectada" + conetar)
---
+++ variables.js
@@ -0,0 +1,18 @@
+//string 
+var sena = "ADSO";
+console.log(sena);
+//numeros
+let numeroEntero = 17;
+var numeroNegativo = -4;
+let numeroDecinmal = 11.74;
+//Booleanos true-false
+var estoyEstudiando = true;
+let estoyAbuerrida = false;
+//undifined
+var num;
+console.log(num);
+num = 0;
+console.log(num);
+//null
+var sinNada = nu11;
+console.log(sinNada)
